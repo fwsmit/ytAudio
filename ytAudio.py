6587de09a96c215493c9ea4cc35a49daa2d52493
Friso Smit <fw.smit01@gmail.com>
2022-11-16 18:27:27 +0100

    Add tags via a postprocessor

--- ytAudio.py
+++ ytAudio.py
@@ -3,12 +3,34 @@ import json
 import yt_dlp
 import tempfile
 import urllib.request
+import glob
+import eyed3
+
+# ℹ️ See help(yt_dlp.postprocessor.PostProcessor)
+class MyCustomPP(yt_dlp.postprocessor.PostProcessor):
+    def __init__(self, artist, album):
+        yt_dlp.postprocessor.PostProcessor.__init__(self)
+        self.artist = artist
+        self.album = album
+
+    def run(self, info):
+        filepath = info.get("filepath")
+        audiofile = eyed3.load(filepath)
+        audiofile.tag.artist = self.artist
+        audiofile.tag.album = self.album
+
+        # Extract the title from the filename
+        audiofile.tag.title = os.path.basename(os.path.splitext(filepath)[0])
+        audiofile.tag.save()
+        self.to_screen('Added metadata')
+        return [], info
 
 def runYtAudio(artist, album, cover_url, url, destdir, testing=False):
     ydl_opts = {
             'format': 'mp3/bestaudio/best',
             'outtmpl': '%(title)s.%(ext)s',
             'test': testing,
+            'embed-metadata': True,
             'postprocessors': [{  # Convert to mp3
                 'key': 'FFmpegExtractAudio',
                 'preferredcodec': 'mp3',
@@ -20,6 +42,7 @@ def runYtAudio(artist, album, cover_url, url, destdir, testing=False):
     os.chdir(destination)
     
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.add_post_processor(MyCustomPP(artist, album), when='post_process')
         info = ydl.extract_info(url, download=True)
 
     urllib.request.urlretrieve(cover_url, "Folder.jpg")
