355316521b428f4ee682eef0437b888374a42242
Friso Smit <fw.smit01@gmail.com>
2022-11-16 18:56:04 +0100

    Remove old unused code

--- ytList.py
+++ ytList.py
@@ -9,18 +9,6 @@ parser.add_argument('--destdir', type=str, help="Destination directory")
 parser.add_argument('--list', type=str, required=True, help="Excel sheet with the download info")
 args = parser.parse_args()
 
-# def runYtAudio(artist, album, cover_url, url, destdir, tempdir):
-    # command_line = ["./ytAudio", "-a", artist, "-A", album, "-c", cover_url]
-    # if destdir:
-        # command_line.extend(["-d", destdir])
-
-    # if tempdir:
-        # command_line.extend(["-t", tempdir])
-
-    # command_line.append(url)
-    # print(command_line)
-    # subprocess.run(command_line)
-
 destdir = args.destdir
 tmpdir = args.tmpdir
 list = args.list
